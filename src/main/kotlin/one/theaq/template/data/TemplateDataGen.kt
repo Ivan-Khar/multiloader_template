8c8fa60c81f031763b80ba8ee69a150ed9583bea
Ivan-Khar <47220198+Ivan-Khar@users.noreply.github.com>
2026-07-24 17:11:26 +0300

    datagen fixes

--- src/main/kotlin/one/theaq/template/data/TemplateDataGen.kt
+++ src/main/kotlin/one/theaq/template/data/TemplateDataGen.kt
@@ -8,7 +8,7 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
 @Entrypoint("fabric-datagen")
 class TemplateDataGen : DataGeneratorEntrypoint {
     override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
-
+        val pack = fabricDataGenerator.createPack()
     }
 }
 //?} else {
